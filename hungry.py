ee457ef66e47d448aa4f73fb287d3dca9568a6ec
Broody_Boy <arshdeep2875@gmail.com>
2023-04-11 03:30:47 +0530

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -1,1 +1,10 @@
-print("I'm hungry")
+
+# original:
+# print("I'm hungry")
+
+# Modified:
+hungry = input("Are you hungry?")
+if hungry == "yes":
+    print("eat Samosa")
+else
+    print("do you homework")
